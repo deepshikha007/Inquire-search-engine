01f5360bea9d0ab60be522b9f307563ce3107dd2
deepshikha007 <deepshikhachauhan773@gmail.com>
2021-07-12 17:25:31 +0530

    inquire

--- distance_proj/settings.py
+++ distance_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yf(1rz^244dff9umm@fvbyn54_k4!(u+kdv0pazu9lfcu_yoao'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
